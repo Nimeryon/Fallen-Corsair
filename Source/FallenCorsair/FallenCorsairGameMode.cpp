6b20b3f3ac369629848918264de5835938a59418
Thomas Auriol <t.auriol46@gmail.com>
2023-03-20 17:12:04 +0100

    First clean third person commit

---
+++ Source/FallenCorsair/FallenCorsairGameMode.cpp
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "FallenCorsairGameMode.h"
+#include "FallenCorsairCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+AFallenCorsairGameMode::AFallenCorsairGameMode()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}

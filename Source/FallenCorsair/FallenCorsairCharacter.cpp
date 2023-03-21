2a4b7f6b9721d639ccfcb6bad353e10b4b669727
MangaLoverDev <83613767+AriiGateau@users.noreply.github.com>
2023-03-21 10:58:02 +0100

    remove jump and add offset camera

--- Source/FallenCorsair/FallenCorsairCharacter.cpp
+++ Source/FallenCorsair/FallenCorsairCharacter.cpp
@@ -74,9 +74,10 @@ void AFallenCorsairCharacter::SetupPlayerInputComponent(class UInputComponent* P
 	// Set up action bindings
 	if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent)) {
 		
+
 		//Jumping
-		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Triggered, this, &ACharacter::Jump);
-		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Completed, this, &ACharacter::StopJumping);
+		//EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Triggered, this, &ACharacter::Jump);
+		//EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Completed, this, &ACharacter::StopJumping);
 
 		//Moving
 		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AFallenCorsairCharacter::Move);

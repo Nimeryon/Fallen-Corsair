ba6293d0c9e91227a2e7b0334cd59db2dffaac4f
MangaLoverDev <83613767+AriiGateau@users.noreply.github.com>
2023-04-11 10:20:30 +0200

    Create Charge class

---
+++ Source/FallenCorsair/Player/Charge.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Charge.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FALLENCORSAIR_API UCharge : public UCharacterMovementComponent
+{
+	GENERATED_BODY()
+	
+};

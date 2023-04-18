1bbe9eae5115c64fb4506127d1030e0b1eec4f5d
kevin-blanchet <kevin.blanchet@ynov.com>
2023-04-18 17:28:55 +0200

    Corrections and polish (demo scene and spawn)

--- Source/FallenCorsair/Waves/WaveZone.cpp
+++ Source/FallenCorsair/Waves/WaveZone.cpp
@@ -13,7 +13,7 @@ AWaveZone::AWaveZone(): m_player(nullptr), m_bIsPlayerInZone(false)
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-	CreateDefaultSubobject<UWaveTracker>(TEXT("WaveTracker"));
+	m_waveTracker = CreateDefaultSubobject<UWaveTracker>(TEXT("WaveTracker"));
 }
 
 // Called when the game starts or when spawned

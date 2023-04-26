9e363c7379644174629defd55a9c4b9ccdf7d478
MangaLoverDev <83613767+AriiGateau@users.noreply.github.com>
2023-04-26 17:15:01 +0200

    fix

--- Source/FallenCorsair/FallenCorsairCharacter.h
+++ Source/FallenCorsair/FallenCorsairCharacter.h
@@ -154,16 +154,18 @@ public:
 	UPROPERTY()
 	FOnAim OnDodge;
 
+	UPROPERTY(BlueprintReadWrite)
+	float m_currentHealth = 50.f;
+
 private:
 
-#pragma region Camera Variables
+#pragma region Melee
 
 
 	// Chrono for melee input
 	float Melee_TriggeredSeconds = 0;
 	bool Melee_IsTrigerred = false;
 #pragma endregion 
-
 
 #pragma region Camera Variables
 	UPROPERTY()
@@ -267,7 +269,10 @@ private:
 	bool m_bCanBeDammage = true;
 
 	UPROPERTY()
-	float m_currentHealth = 50.f;
+	bool m_bIsLowHP = false;
+
+	// UPROPERTY()
+	// float m_currentHealth = 50.f;
 
 	UPROPERTY()
 	float m_alphaRecover = 1.f;
@@ -290,8 +295,8 @@ private:
 	UPROPERTY(EditAnywhere, Category = "Vie", meta = (displayName = "Vie du joueur Low"), meta = (ClampMin = 0, UIMin = 0, ClampMax = 100, UIMax = 100))
 	float m_lowHP = 20.f;
 
-	UPROPERTY(EditAnywhere, Category = "Vie", meta = (displayName = "Fade Speed"), meta = (ClampMin = 1, UIMin = 1, ClampMax = 10, UIMax = 10))
-	float m_changeSpeed = 20.f;
+	UPROPERTY(EditAnywhere, Category = "Vie", meta = (displayName = "Fade Speed"), meta = (ClampMin = 0.1, UIMin = 0.1, ClampMax = 100, UIMax = 100))
+	float m_changeSpeed = 1.f;
 
 	UPROPERTY(EditAnywhere, Category = "Vie", meta = (displayName = "MPC"))
 	UMaterialParameterCollection* m_collection;
